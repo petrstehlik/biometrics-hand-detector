3a320f79ccfbbec0676e7f63d8f8ecbe9731caec
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-11-16 09:23:30 +0000

    Add settings to camera setup

--- src/capture.cpp
+++ src/capture.cpp
@@ -1,14 +1,18 @@
 #include <pylon/PylonIncludes.h>
+#include <pylon/gige/BaslerGigEInstantCamera.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/video/video.hpp>
 
-static const size_t COUNT_OF_IMAGES = 1024;
-static const int TIMEOUT_MS = 10000; // TODO not used currently
+static const size_t COUNT_OF_IMAGES = 32;
+static const int TIMEOUT_MS = 1000; // TODO not used currently
+
+static const int ACQUISITION_RATE = 1000;
+static const int EXPOSURE_TIME = 1000;
 
 static const int ROWS = 256;
-static const int COLS = 2048;
+static const int COLS = 6144;
 
 using namespace Pylon;
 using namespace std;
@@ -20,10 +24,17 @@ int main(int argc, char* argv[])
 
 	try {
 		Mat resultImage = Mat(ROWS, COLS, CV_8UC3);
-		CInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice());
+        	CBaslerGigEInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice());
 		CGrabResultPtr ptrGrabResult;
 
 		cout << "Using device " << camera.GetDeviceInfo().GetModelName() << endl;
+
+		camera.Open();
+		camera.Width.SetValue(COLS);
+		camera.Height.SetValue(ROWS);
+
+		camera.ExposureTimeRaw.SetValue(EXPOSURE_TIME);
+		camera.AcquisitionLineRateAbs.SetValue(ACQUISITION_RATE);
 
 		camera.StartGrabbing(COUNT_OF_IMAGES);
 
@@ -40,6 +51,7 @@ int main(int argc, char* argv[])
 				currentImage = Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC3, (uint8_t *) pylonImage.GetBuffer());
 				resultImage.push_back(currentImage);
 			}
+			cout << "captured" << endl;
 		}
 
 		imwrite("result.jpg", resultImage);

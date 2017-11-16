3c4fb0044703d6d3f69b0c41b74df51dcd2375b3
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-11-16 20:48:40 +0000

    Add binary run to the capture script

--- rail-control/capture.py
+++ rail-control/capture.py
@@ -2,21 +2,23 @@ import tel_commander as light
 import serial as serial
 import time
 import logging
+import shutil
+import subprocess
 
 logging.basicConfig(level = logging.DEBUG)
 log = logging.getLogger("CAPTURE")
 
 log.info("Setting up serial")
-arduino = serial.Serial('/dev/ttyUSB0', 115200)
+ser = serial.Serial('/dev/ttyUSB0', 115200)
 
 if __name__ == "__main__":
     # Send rail for 10s left
     log.info("Sending rail left for 10 s")
-    ser.write("L10000")
+    ser.write("R10000")
 
     # Set light intensity to 1200 mA
     log.info("Set light intensity to 1200 mA")
-    light.cmd("IY E 1200")
+    light.cmd("IY E 1400")
 
     # Turn light on
     log.info("Turning light on")
@@ -25,10 +27,13 @@ if __name__ == "__main__":
     # Here should be runing of the grabbing binary
     log.debug("Should grab image")
     log.info("Waiting for movement finish")
-    time.sleep(10.0)
+    subprocess.call("../src/capture 700 500", shell=True)
+
+    shutil.copyfile("./result.jpg", "/var/www/html/bio/result_"+str(int(time.time()))+".jpg")
+
 
     log.info("Turning off light")
     light.cmd("LC A 0")
 
     log.info("Sending rail back right for 10 s")
-    ser.write("R10000")
+    ser.write("L10000")

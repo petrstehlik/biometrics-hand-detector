c8791de94d2c26c9a6175aca64d59dfd07b980e1
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-11-16 20:49:50 +0000

    Fix deformation of captured image

--- src/capture.cpp
+++ src/capture.cpp
@@ -7,7 +7,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/video/video.hpp>
 
-static const size_t COUNT_OF_IMAGES = 32;
+static const size_t COUNT_OF_IMAGES = 20;
 static const int TIMEOUT_MS = 1000; // TODO not used currently
 
 static const int DEFAULT_ACQUISITION_RATE = 1000;
@@ -29,15 +29,17 @@ int main(int argc, char* argv[])
 	int acquisition_rate = DEFAULT_ACQUISITION_RATE;
 	int exposure_time = DEFAULT_EXPOSURE_TIME;
 
-	if (argc > 2)
+	if (argc >= 2)
 		exposure_time = stoi(string(argv[1]));
-	if (argc > 3)
+	if (argc >= 3)
 		acquisition_rate = stoi(string(argv[2]));
 
 	PylonInitialize();
 
 	try {
 		Mat resultImage = Mat(ROWS, COLS, CV_8UC3);
+		Mat resizedImage = Mat(ROWS*2, COLS, CV_8UC3);
+
         	CBaslerGigEInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice());
 		CGrabResultPtr ptrGrabResult;
 
@@ -46,6 +48,8 @@ int main(int argc, char* argv[])
 		camera.Open();
 		camera.Width.SetValue(COLS);
 		camera.Height.SetValue(ROWS);
+		camera.OffsetX.SetValue(0);
+		camera.OffsetY.SetValue(0);
 
 		cout << "Setting exposure time to " << to_string(exposure_time) << endl;
 		camera.ExposureTimeRaw.SetValue(exposure_time);
@@ -70,7 +74,10 @@ int main(int argc, char* argv[])
 			cout << "captured" << endl;
 		}
 
-		imwrite("result.jpg", resultImage);
+		resize(resultImage, resizedImage, Size(COLS*2, ROWS * COUNT_OF_IMAGES));
+
+		imwrite("result.jpg", resizedImage);
+		//imwrite("result.jpg", resultImage);
 
 	}
 	catch(const GenericException &e)
